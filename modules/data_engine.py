4707aedfd2f07a2cf28222f4720d3f6931d0fea3
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-02 12:12:33 +0530

    Update data_engine.py

--- modules/data_engine.py
+++ modules/data_engine.py
@@ -1,1 +1,8 @@
+import yfinance as yf
 
+def get_history(symbol, period="1y"):
+    df = yf.download(symbol, period=period, auto_adjust=True)
+    return df.dropna()
+
+def get_info(symbol):
+    return yf.Ticker(symbol).info
