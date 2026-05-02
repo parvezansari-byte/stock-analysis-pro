2a354dedf3bff412d8c0049df08a5408a1bfea8d
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-02 12:05:06 +0530

    Add functions to fetch stock data using yfinance

--- data_engine.py
+++ data_engine.py
@@ -1,1 +1,7 @@
+import yfinance as yf
 
+def get_history(symbol, period="1y"):
+    return yf.download(symbol, period=period, auto_adjust=True)
+
+def get_info(symbol):
+    return yf.Ticker(symbol).info
