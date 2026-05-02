96670cd7388dff8d9116aa48db6b3626b58d7ddc
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-02 11:52:10 +0530

    Update fundamental.py

--- modules/fundamental.py
+++ modules/fundamental.py
@@ -1,1 +1,8 @@
+def extract_fundamentals(info):
 
+    return {
+        "pe": info.get("trailingPE"),
+        "roe": info.get("returnOnEquity"),
+        "de": info.get("debtToEquity"),
+        "growth": info.get("revenueGrowth"),
+    }
