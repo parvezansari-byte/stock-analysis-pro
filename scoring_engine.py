1db60a1a9563b256320bf6461d0a563965405d8b
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-02 12:11:32 +0530

    Delete scoring_engine.py

--- scoring_engine.py
+++
@@ -1,8 +0,0 @@
-def score_stock(m):
-    score = 0
-
-    if m["close"] > m["sma20"]: score += 20
-    if m["close"] > m["sma50"]: score += 20
-    if 50 < m["rsi"] < 70: score += 20
-
-    return score
