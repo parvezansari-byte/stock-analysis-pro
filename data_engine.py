f9296dae64178930b8d2b62a322fdb5f65a3fb9a
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-02 12:11:20 +0530

    Delete data_engine.py

--- data_engine.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-
-def get_history(symbol, period="1y"):
-    return yf.download(symbol, period=period, auto_adjust=True)
-
-def get_info(symbol):
-    return yf.Ticker(symbol).info
